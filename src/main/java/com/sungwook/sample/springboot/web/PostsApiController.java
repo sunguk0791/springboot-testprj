1dbd832f9c858a43a64922f1b3c1c19949d55ed4
sungwook <destiny0810@naver.com>
2020-06-22 15:34:50 +0900

    3rd version.

---
+++ src/main/java/com/sungwook/sample/springboot/web/PostsApiController.java
@@ -0,0 +1,23 @@
+package com.sungwook.sample.springboot.web;
+import com.sungwook.sample.springboot.service.posts.PostsService;
+import com.sungwook.sample.springboot.web.dto.PostsSaveRequestDto;
+import com.sungwook.sample.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+}
---
+++ src/main/java/com/sungwook/sample/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.sungwook.sample.springboot.web.dto;
+
+import com.sungwook.sample.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
